e5a7c9b86fcaaa490c526f9632ee0d23ecb6185d
anoopanilpa <88606953+anoopanilpa@users.noreply.github.com>
2024-03-20 04:59:30 +0530

    Update check_IP_from_sg.sh
    IPs list added

--- check_IP_from_sg.sh
+++ check_IP_from_sg.sh
@@ -2,8 +2,8 @@
 
 regions=$(aws ec2 describe-regions --query 'Regions[*].RegionName' --output text)
 
-# Your IP address to check
-MY_IP="IP here"
+# Your IP address to check seperate by comma
+MY_IP=192.168.10.12,192.168.10.11,192.168.10.13
 
 for region in $regions; do
  
@@ -14,15 +14,17 @@ SECURITY_GROUPS=$(aws ec2 describe-security-groups --region "$region" --query 'S
   for sg_id in $SECURITY_GROUPS ; do
       #echo "Checking security group: $sg_id"
       # Describe ingress rules of the security group
+   for i in $(echo $MY_IP | sed "s/,/ /g"); do 
       aws ec2 describe-security-groups --region $region --group-ids "$sg_id" --query 'SecurityGroups[].IpPermissions[]' --output text | \
       while read -r line; do
           # Check if your IP is whitelisted
-          if echo "$line" | grep -q "$MY_IP"; then
-              echo "Your IP $MY_IP is whitelisted in security group $sg_id region $region"
+          if echo "$line" | grep -q "$i"; then
+              echo "Your IP $i is whitelisted in security group $sg_id region $region"
               # You can exit here if you only want to know if your IP is whitelisted in any of the security groups.
               # exit 0
           fi
       done
+   done   
   done
 
 done
