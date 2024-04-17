bab5f83796e7983928a798a75456ac3118ec3f35
anoopanilpa <88606953+anoopanilpa@users.noreply.github.com>
2024-04-17 19:48:58 +0530

    adding as new

--- aws_check_ebs_size_ec2.sh
+++ aws_check_ebs_size_ec2.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 # List all EBS volumes, their sizes, and attached instance (if any)
-echo "Volume ID     Size (GB)    Attached Instance ID"
-echo "==============================================="
+#echo "Volume ID     Size (GB)    Attached Instance ID"
+#echo "==============================================="
+
+regions=$(aws ec2 describe-regions --query 'Regions[*].RegionName' --output text)
+
+for region in $regions; do
+
 
 # Get volume information
-aws  --profile mine ec2 describe-volumes --query 'Volumes[*].[VolumeId,Size,Attachments[0].InstanceId]' --output text | \
-while read -r volume_id size instance_id; do
+ aws ec2 describe-volumes --region "$region" --query 'Volumes[*].[VolumeId,Size,Attachments[0].InstanceId]' --output text | \
+ while read -r volume_id size instance_id; do
     if [ -z "$instance_id" ]; then
         instance_id="Not Attached"
     fi
     # Convert size to GB
     size_gb=$((size / 1024))
-    echo "$volume_id     $size     $instance_id"
+    echo "$volume_id,$instance_id,$size"
+ done
+
 done
